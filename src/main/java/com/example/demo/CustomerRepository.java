73b6374a64b77803273d7c7bc8ebe7fb13a10f8e
ecnuhail <2596124342@qq.com>
2021-03-23 00:09:40 +0800

    write

---
+++ src/main/java/com/example/demo/CustomerRepository.java
@@ -0,0 +1,15 @@
+package com.example.demo;
+
+
+
+import java.util.List;
+
+import org.springframework.data.mongodb.repository.MongoRepository;
+
+public interface CustomerRepository extends MongoRepository<Customer, String> {
+
+    public Customer findByFirstName(String firstName);
+    public List<Customer>  findByFirstNameAndLastName(String firstName, String lastName);
+    public List<Customer> findByLastName(String lastName);
+
+}
